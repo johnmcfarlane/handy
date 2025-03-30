3505810f086b6ff9d5f06e4678f5fd02d815875e
John McFarlane <git@john.mcfarlane.name>
2025-03-30 13:22:04 +0100

    Assortment
    - Remove old Docker scripts.
    - Replace get-{compiler}-head scripts with
      get-{compiler} scripts
      - with all sorts of fixes to build recent compiler
      - don't pull latest as iteration is SLOW!

--- docker-make.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker build --tag dev ~/bin
-docker create -t --name dev -i dev
--- docker-nuke.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
